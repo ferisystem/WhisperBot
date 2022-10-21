41f2ccf39731d4117c2d36c84573d52fca50efcf
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-10-21 20:01:08 +0330

    fix bug

--- bot.py
+++ bot.py
@@ -2356,7 +2356,7 @@ def generate_uniqid():
 	text = ''.join(random.choices(string.ascii_letters + string.digits, k=29))
 	while True:
 		if not DataBase.sismember('file_ids', text):
-			DataBase.sadd('file_ids', text):
+			DataBase.sadd('file_ids', text)
 			break
 		text = ''.join(random.choices(string.ascii_letters + string.digits, k=29))
 	return text
