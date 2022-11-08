fd853ea688f4afbd4a12f8289292d90f13977c24
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-11-08 04:00:28 +0330

    add inline_msg_id to DB when whispering as reply as anonymous admin

--- whisperbot/callback_process.py
+++ whisperbot/callback_process.py
@@ -1547,7 +1547,7 @@ async def callback_query_process(msg: types.CallbackQuery):
                         ),
                     )
                 await answerCallbackQuery(
-                    msg, text_data, show_alert=True, cache_time=3600
+                    msg, text_data, show_alert=True,# cache_time=3600
                 )
                 if (
                     DataBase.scard(
@@ -1602,6 +1602,15 @@ async def callback_query_process(msg: types.CallbackQuery):
                             ),
                             int(time()),
                         )
+                        msg_inline = msg.inline_message_id
+                        if DataBase.hget(
+                            f"setting_najva:{from_user}",
+                            "autodel"
+                        ):
+                            DataBase.sadd(
+                                "najva_autodel",
+                                f"{from_user}:{time_data}:{msg_inline}",
+                            )
                 DataBase.incr(
                     "najva_seen_count:{}:{}".format(from_user, time_data)
                 )
@@ -1613,7 +1622,7 @@ async def callback_query_process(msg: types.CallbackQuery):
                     msg,
                     langU["najva_not_for_you"],
                     show_alert=True,
-                    cache_time=3600,
+                    # cache_time=3600,
                 )
         if re.match(r"^delnajva:(\d+):([-+]?\d*\.\d+|\d+)$", input):
             ap = re_matches(r"^delnajva:(\d+):([-+]?\d*\.\d+|\d+)$", input)
