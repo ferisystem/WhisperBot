906c41bc20724cdb17ae0f5955d0066ca2a86138
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-12-23 20:42:55 +0330

    reset IDs with python command line args

--- bot_main.py
+++ bot_main.py
@@ -3,13 +3,24 @@ from whisperbot.switch_func import bot_run, bot_off
 from core_file import (
     get_new_configured_app,
     GlobalValues,
+    rds,
     ssl,
     web,
-    dp
+    dp,
+    db
 )
+import sys
 # -------------------------------------------------------------------------------- #
 # کاراکتر
 if __name__ == "__main__":
+    if sys.argv[1] == "reset":
+        rds.delete(db)
+    elif sys.argv[1] == "set-support-gp":
+        rds.hdel(db, "supchat")
+    elif sys.argv[1] == "set-logs-ch":
+        rds.hdel(db, "logchat")
+    elif sys.argv[1] == "set-linky-ch":
+        rds.hdel(db, "linkyCH")
     app = get_new_configured_app(dispatcher=dp, path=GlobalValues().WEBHOOK_URL_PATH)
     app.on_startup.append(bot_run)
     app.on_shutdown.append(bot_off)
